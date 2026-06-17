ca38bc4eb344f74dd64c1298e949ed1c7c151efa
gatoott <arfanurhalim474@gmail.com>
2026-06-17 08:17:41 +0700

    menambahka component atas halaman

---
+++ src/assets/components/atas.jsx
@@ -0,0 +1,9 @@
+export default function Atas() {
+    return (
+        <header>
+            <div class="badge">IP Intelligence</div>
+            <h1>Lacak <span>Alamat IP</span></h1>
+            <p class="subtitle">Masukkan IP atau kosongkan untuk cek IP Anda sendiri</p>
+        </header>
+    )
+}
